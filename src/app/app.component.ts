f28ad39c5649803a46841f8f8bc9c97ec5abeed4
unknown <diego.caceres@ucb.edu.bo>
2024-07-18 09:35:55 -0400

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,32 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {CommonModule} from "@angular/common";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatButtonModule,MatIconModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'Juegos-digitales';
+  estaJugando :boolean;
+  departamento: string = ""
+  constructor() {
+    this.estaJugando = false
+  }
+
+  activarJuego(departamento: string){
+    this.estaJugando = true;
+    this.departamento = departamento;
+    console.log(this.estaJugando)
+    console.log(this.departamento)
+  }
+
+  volverAlMenu(){
+    this.estaJugando = false;
+  }
 }
